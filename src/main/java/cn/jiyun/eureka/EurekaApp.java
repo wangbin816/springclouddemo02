2d12763cb62e1858d72c0a0fa5820c4afe32e027
wangbin816 <332545767@qq.com>
2020-11-01 21:39:27 +0800

    我把操作改成了3333

--- src/main/java/cn/jiyun/eureka/EurekaApp.java
+++ src/main/java/cn/jiyun/eureka/EurekaApp.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApp {
-//123123
+//333
     public static void main(String[] args){
         SpringApplication.run(EurekaApp.class,args);
     }
